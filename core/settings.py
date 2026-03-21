09276abb7e3c616fce428d9a4074fa6339eb88be
Luis <otavio.cdomingos@gmail.com>
2026-03-21 20:58:53 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['giving-charm-production-d03a.up.railway.app']
 
 
 # Application definition
