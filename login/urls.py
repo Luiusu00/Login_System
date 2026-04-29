b96abf1ce5f5ef866feb52ed1a5af6d90954ed9f
Luis <otavio.cdomingos@gmail.com>
2026-04-29 15:24:59 -0300

    Update urls.py

--- login/urls.py
+++ login/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.telaLogin, name='telaLogin'),
     path('sistema/', views.sistema, name='sistema'),
     path('new-password/', views.newPassword, name='new-password'),
+    path('logout/', views.logout, name='logout'),
 
 ]
